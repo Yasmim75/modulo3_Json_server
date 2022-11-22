f6a5122fc4a13b74308f302b114ba6bfd8f19fa0
Yasmim Alves <113534750+Yasmim75@users.noreply.github.com>
2022-11-22 11:41:15 -0300

    Add files via upload
    atualização

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const jsonServer = require('json-server');
 const server = jsonServer.create();
-const router = jsonServer.router('db.json'); 
+const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3200;
+
+const port = process.env.PORT || 8080;
 
 server.use(middlewares);
-server.use(router);
+server.use(jsonServer.rewriter({
+  "/todos_os_imobiliaria": "/imobiliaria",
+  "/adicionar_imobiliaria": "/imobiliaria",
+  "/editar_imobiliaria/:id": "/imobiliaria/:id",
+  "/deletar_imobiliaria/:id": "/imobiliaria/:id"
 
-server.listen(port);
+}))
+server.use(router);
+server.listen(port, () => { console.log(`JSON Server está rodando na porta ${port}`) })
